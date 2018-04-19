2cf95b8781cde453fb725fb5fadf1723bedb1e35
Matt Trussell <smtrussell@gmail.com>
2018-04-19 14:12:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    # pig = PigLatinizer.new
+    pig = PigLatinizer.new
     # @pig_phrase = pig.piglatinize(params[:user_input])
     erb :results
   end
